5dcd5db489f4c489b4733272ad0fcb89fca09c7b
CMarcoo <cmarco.org@gmail.com>
2024-02-07 00:29:35 +0100

    Initial commit

---
+++ src/main/java/top/cmarco/aequitas/Aequitas.java
@@ -0,0 +1,17 @@
+package top.cmarco.aequitas;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Aequitas extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
